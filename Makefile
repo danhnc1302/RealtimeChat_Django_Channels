94fd2135e129bb6cc56ffb7a8b49dcaa4476be98
danhnc <danhnc1302@gmail.com>
2024-01-27 00:43:13 +0700

    Fixed kotlin compile

---
+++ Makefile
@@ -0,0 +1,6 @@
+
+run-android:
+	npm run android
+
+run-server:
+	cd api && python manage.py runserver
--- android/app/src/main/java/com/realtimechat_django_channels/MainApplication.kt
+++ android/app/src/main/java/com/realtimechat_django_channels/MainApplication.kt
@@ -11,6 +11,7 @@ import com.facebook.react.defaults.DefaultReactHost.getDefaultReactHost
 import com.facebook.react.defaults.DefaultReactNativeHost
 import com.facebook.react.flipper.ReactNativeFlipper
 import com.facebook.soloader.SoLoader
+import com.realtimechat_django_channels.BuildConfig
 
 class MainApplication : Application(), ReactApplication {
 
